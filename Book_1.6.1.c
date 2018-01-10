0cc5702eefaf76969ca20dd6d330c3e3d37e8f33
Jonathan Tang <jonathanjytang@gmail.com>
2018-01-10 11:46:28 +0800

    Add Book_1.6.1.c

---
+++ Book_1.6.1.c
@@ -0,0 +1,48 @@
+/**UVa Online Judge ID 100: The 3n + 1 Problem
+Run time on UVa Online Judge: 0.320 seconds*/
+
+#include <stdio.h>
+
+int main(){
+    /*Use unsigned long int since numbers range between 0 and 1 000 000*/
+    unsigned long int origI, origJ, i, j, num, n;
+    unsigned long int maxCycleLength, currentCycleLength;
+
+    while(scanf("%ld %ld",&origI,&origJ) == 2){ /*This part will not be true after EOF has been reached*/
+        maxCycleLength = 1; /*Reset this variable for each line of input*/
+
+        /* If origI > origJ, swap i and j so that i < j*/
+        if(origI > origJ){
+            i = origJ;
+            j = origI;
+        }
+        else{
+            i = origI;
+            j = origJ;
+        }
+
+        /*Iterate over numbers between i and j (inclusive) to find maxCycleLength*/
+        for(num = i; num <= j; num++){
+            n = num;
+            currentCycleLength = 1; /*Reset this variable for each number between i and j*/
+
+            /*Code to follow the 3n+1 calculation sequence*/
+            while(n != 1){
+                if(n % 2 == 0){ /*If n is even*/
+                    n = n/2;
+                }
+                else{ /*If n is odd*/
+                    n = 3*n + 1;
+                }
+                currentCycleLength++;
+            }
+
+            if(currentCycleLength > maxCycleLength){
+                /*Store new cycle length only if it's longer than the previous longest cycle length*/
+                maxCycleLength = currentCycleLength;
+            }
+        }
+        printf("%ld %ld %ld\n",origI,origJ,maxCycleLength); /*print output after processing a line of input*/
+    }
+    return 0;
+}
