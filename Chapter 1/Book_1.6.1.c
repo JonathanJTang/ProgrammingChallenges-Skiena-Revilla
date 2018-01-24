6c6fb8fa5bd2c5084334762f281864b3bb11a23c
Jonathan Tang <jonathanjytang@gmail.com>
2018-01-24 18:05:36 +0000

    Update Book_1.6.1.c

--- Chapter 1/Book_1.6.1.c
+++ Chapter 1/Book_1.6.1.c
@@ -8,8 +8,8 @@ int main(){
     unsigned long int origI, origJ, i, j, num, n;
     unsigned long int maxCycleLength, currentCycleLength;
 
-    while(scanf("%ld %ld",&origI,&origJ) == 2){ /*This part will not be true after EOF has been reached*/
-        maxCycleLength = 1; /*Reset this variable for each line of input*/
+    while(scanf("%ld %ld",&origI,&origJ) == 2){  /*This part will not be true after EOF has been reached*/
+        maxCycleLength = 1;  /*Reset this variable for each line of input*/
 
         /* If origI > origJ, swap i and j so that i < j*/
         if(origI > origJ){
@@ -24,14 +24,14 @@ int main(){
         /*Iterate over numbers between i and j (inclusive) to find maxCycleLength*/
         for(num = i; num <= j; num++){
             n = num;
-            currentCycleLength = 1; /*Reset this variable for each number between i and j*/
+            currentCycleLength = 1;  /*Reset this variable for each number between i and j*/
 
             /*Code to follow the 3n+1 calculation sequence*/
             while(n != 1){
-                if(n % 2 == 0){ /*If n is even*/
+                if(n % 2 == 0){  /*If n is even*/
                     n = n/2;
                 }
-                else{ /*If n is odd*/
+                else{  /*If n is odd*/
                     n = 3*n + 1;
                 }
                 currentCycleLength++;
@@ -42,7 +42,7 @@ int main(){
                 maxCycleLength = currentCycleLength;
             }
         }
-        printf("%ld %ld %ld\n",origI,origJ,maxCycleLength); /*print output after processing a line of input*/
+        printf("%ld %ld %ld\n",origI,origJ,maxCycleLength);  /*print output after processing a line of input*/
     }
     return 0;
 }
